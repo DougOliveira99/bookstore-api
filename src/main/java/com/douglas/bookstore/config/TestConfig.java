ef5c935c031205534ce3e4e749aca4a01c94f024
Douglas Oliveira <douglas.toliveira@hotmail.com>
2022-06-12 18:57:22 -0300

    Test profile updated and database seeding

--- src/main/java/com/douglas/bookstore/config/TestConfig.java
+++ src/main/java/com/douglas/bookstore/config/TestConfig.java
@@ -1,37 +1,22 @@
 package com.douglas.bookstore.config;
 
-import java.util.Arrays;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-import com.douglas.bookstore.domain.Book;
-import com.douglas.bookstore.domain.Category;
-import com.douglas.bookstore.repositories.BookRepository;
-import com.douglas.bookstore.repositories.CategoryRepository;
+import com.douglas.bookstore.service.DBService;
 
 @Configuration
 @Profile("test")
-public class TestConfig implements CommandLineRunner {
+public class TestConfig {
 	
 	@Autowired
-	private CategoryRepository categoryRepository;
+	private DBService dbService;
 	
-	@Autowired
-	private BookRepository bookRepository;
-
-	@Override
-	public void run(String... args) throws Exception {
-
-		Category cat1 = new Category(null, "Computers", "IT books");
-
-		Book b1 = new Book(null, "Clean code", "Robert Martin", "Lorem ipsum", cat1);
-
-		cat1.getBooks().addAll(Arrays.asList(b1));
-		
-		categoryRepository.saveAll(Arrays.asList(cat1));
-		bookRepository.saveAll(Arrays.asList(b1));
+	@Bean
+	public void databaseSeeding() {
+		this.dbService.databaseSeeding();
 	}
+
 }
